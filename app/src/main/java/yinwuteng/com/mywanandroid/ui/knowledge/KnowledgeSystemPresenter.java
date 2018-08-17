38b950e3ec136f9e15b7fc06f206a3ebe5920301
yinwuteng <470047349@qq.com>
2018-08-17 10:05:42 +0800

    branch_test

--- app/src/main/java/yinwuteng/com/mywanandroid/ui/knowledge/KnowledgeSystemPresenter.java
+++ app/src/main/java/yinwuteng/com/mywanandroid/ui/knowledge/KnowledgeSystemPresenter.java
@@ -5,19 +5,25 @@ import android.annotation.SuppressLint;
 import java.util.List;
 
 import io.reactivex.functions.Consumer;
-import yinwuteng.com.mywanandroid.base.BasePresenter;
-import yinwuteng.com.mywanandroid.bean.DataResponse;
-import yinwuteng.com.mywanandroid.bean.KnowledgeSystem;
+import yinwuteng.com.mywanandroid.base
+        .BasePresenter;
+import yinwuteng.com.mywanandroid.bean
+        .DataResponse;
+import yinwuteng.com.mywanandroid.bean
+        .KnowledgeSystem;
 import yinwuteng.com.mywanandroid.net.ApiService;
-import yinwuteng.com.mywanandroid.net.RetrofitManager;
-import yinwuteng.com.mywanandroid.utils.RxSchedulers;
+import yinwuteng.com.mywanandroid.net
+        .RetrofitManager;
+import yinwuteng.com.mywanandroid.utils
+        .RxSchedulers;
 
 /**
  * Create By yinwuteng
  * 2018/4/21.
  * 知识体系控制器
  */
-public class KnowledgeSystemPresenter extends BasePresenter<KnowledgeSystemView> {
+public class KnowledgeSystemPresenter extends
+        BasePresenter<KnowledgeSystemView> {
     public KnowledgeSystemPresenter() {
     }
 
@@ -27,15 +33,22 @@ public class KnowledgeSystemPresenter extends BasePresenter<KnowledgeSystemView>
     @SuppressLint("CheckResult")
     public void loadKnowledgeSystem() {
         getView().showLoading();
-        RetrofitManager.create(ApiService.class).getKnowledgeSystems().compose(RxSchedulers.<DataResponse<List<KnowledgeSystem>>>applySchedulers()).subscribe(new Consumer<DataResponse<List<KnowledgeSystem>>>() {
+        RetrofitManager.create(ApiService
+                .class).getKnowledgeSystems()
+                .compose(RxSchedulers
+                        .<DataResponse<List<KnowledgeSystem>>>applySchedulers()).subscribe(new Consumer<DataResponse<List<KnowledgeSystem>>>() {
             @Override
-            public void accept(DataResponse<List<KnowledgeSystem>> dataResponse) throws Exception {
-                getView().setKnowledgeSystems(dataResponse.getData());
+            public void accept
+                    (DataResponse<List<KnowledgeSystem>> dataResponse) throws Exception {
+                getView().setKnowledgeSystems
+                        (dataResponse.getData());
             }
         }, new Consumer<Throwable>() {
             @Override
-            public void accept(Throwable throwable) throws Exception {
-                getView().showFailed(throwable.getMessage());
+            public void accept(Throwable
+                                       throwable) throws Exception {
+                getView().showFailed(throwable
+                        .getMessage());
             }
         });
     }
